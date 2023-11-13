d58a614e54fc7a4e2c1ab96ccbd419c2512ffc7e
JayG <70147609+jay-gurjargour@users.noreply.github.com>
2023-11-13 07:42:19 +0530

    Init

---
+++ aliases.zsh
@@ -0,0 +1,25 @@
+## Lazy pacman commands because lazy
+#alias pcup="sudo pacman -Syyu"
+#alias pacin="sudo pacman -S "
+#alias pacun="sudo pacman -Rsc "
+#
+## Lazy system commands because lazy
+#alias syspend="loginctl suspend"
+
+# Lazy shorthands because lazy
+alias e="nvim $1"
+alias nvimrc="nvim ~/.config/nvim/"
+alias zshrc="nvim ~/.config/zsh-config/" 
+alias ls="exa --icons"
+alias ll="exa -lah --icons"
+
+# Lazy dir switching because lazy
+alias cde="cd /Storage/BigData/$1"
+alias cdd="cd /Storage/SmallData/$1"
+
+#
+#
+# Aliases based on current work
+#
+#
+
---
+++ exports.zsh
@@ -0,0 +1,3 @@
+## Exports 
+
+export VISUAL=nvim
